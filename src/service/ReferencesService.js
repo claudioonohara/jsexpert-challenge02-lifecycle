5b5b23be2120c2658fac3a777c140f615b62e447
claudio.onohara <claudioonohara@gmail.com>
2021-12-02 23:50:37 -0400

    feat: ensure to make a deep copy reference

--- src/service/ReferencesService.js
+++ src/service/ReferencesService.js
@@ -4,17 +4,13 @@ class ReferencesService {
   }
 
   getCounterWithoutCopyingReferences(object) {
-    // TODO: assing object to a new variable and return it.
-    return null;
+    return {...object};
   }
 
-  /*
-  // @TIP: if you want to do the extra sub-challenge, uncomment and implement the following function:
   getCounterWithoutDeepCopyingReferences(object) {
-    // TODO: assing object to a new variable and return it.
-    return null;
+    const newObject = JSON.parse(JSON.stringify(object))
+    return newObject;
   }
-  */
 }
 
 module.exports = ReferencesService;
