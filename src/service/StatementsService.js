df7da90b66f5319cf8b3eef084b3a7557bdb339f
claudio.onohara <claudioonohara@gmail.com>
2021-12-03 07:22:20 -0400

    feat: ensure to return second condition chaining all conditions

--- src/service/StatementsService.js
+++ src/service/StatementsService.js
@@ -9,7 +9,7 @@ class StatementsService {
 
   getReturnBasedOnStatement3(condition1, condition2, condition3) {
     // TODO: use condition stations && or || in order to return the expected value (chaining all conditions)
-    return null;
+    return condition2 || condition1 && condition3;
   }
 
   getReturnBasedOnStatement4(condition1, condition2, condition3) {
